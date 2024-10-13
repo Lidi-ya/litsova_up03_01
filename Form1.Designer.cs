fe039f13a9efe0b985b8734ac93f13f7ec6364b9
Lidi-ya <167968297+Lidi-ya@users.noreply.github.com>
2024-10-13 16:47:01 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Fotoalbom
+{
+    partial class Form_albom
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView_albom = new System.Windows.Forms.DataGridView();
+            this.albumsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.photoalbumDataSet = new Fotoalbom.PhotoalbumDataSet();
+            this.albumsTableAdapter = new Fotoalbom.PhotoalbumDataSetTableAdapters.AlbumsTableAdapter();
+            this.button_add = new System.Windows.Forms.Button();
+            this.button_red = new System.Windows.Forms.Button();
+            this.button_del = new System.Windows.Forms.Button();
+            this.textBox_nomer_alboma = new System.Windows.Forms.TextBox();
+            this.textBox_polz = new System.Windows.Forms.TextBox();
+            this.textBox_name = new System.Windows.Forms.TextBox();
+            this.textBox_data = new System.Windows.Forms.TextBox();
+            this.label_nomer_alboma = new System.Windows.Forms.Label();
+            this.label_polz = new System.Windows.Forms.Label();
+            this.label_name = new System.Windows.Forms.Label();
+            this.label_data = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_albom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.albumsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.photoalbumDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView_albom
+            // 
+            this.dataGridView_albom.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.dataGridView_albom.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_albom.Location = new System.Drawing.Point(70, 24);
+            this.dataGridView_albom.Name = "dataGridView_albom";
+            this.dataGridView_albom.RowHeadersWidth = 62;
+            this.dataGridView_albom.RowTemplate.Height = 28;
+            this.dataGridView_albom.Size = new System.Drawing.Size(657, 205);
+            this.dataGridView_albom.TabIndex = 0;
+            // 
+            // albumsBindingSource
+            // 
+            this.albumsBindingSource.DataMember = "Albums";
+            this.albumsBindingSource.DataSource = this.photoalbumDataSet;
+            // 
+            // photoalbumDataSet
+            // 
+            this.photoalbumDataSet.DataSetName = "PhotoalbumDataSet";
+            this.photoalbumDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // albumsTableAdapter
+            // 
+            this.albumsTableAdapter.ClearBeforeFill = true;
+            // 
+            // button_add
+            // 
+            this.button_add.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button_add.Font = new System.Drawing.Font("MV Boli", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_add.Location = new System.Drawing.Point(563, 262);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(192, 56);
+            this.button_add.TabIndex = 1;
+            this.button_add.Text = "Добавить";
+            this.button_add.UseVisualStyleBackColor = false;
+            this.button_add.Click += new System.EventHandler(this.button_add_Click);
+            // 
+            // button_red
+            // 
+            this.button_red.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button_red.Font = new System.Drawing.Font("MV Boli", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.button_red.Location = new System.Drawing.Point(563, 363);
+            this.button_red.Name = "button_red";
+            this.button_red.Size = new System.Drawing.Size(192, 55);
+            this.button_red.TabIndex = 2;
+            this.button_red.Text = "Изменить";
+            this.button_red.UseVisualStyleBackColor = false;
+            this.button_red.Click += new System.EventHandler(this.button_red_Click);
+            // 
+            // button_del
+            // 
+            this.button_del.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button_del.Font = new System.Drawing.Font("MV Boli", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.button_del.Location = new System.Drawing.Point(563, 458);
+            this.button_del.Name = "button_del";
+            this.button_del.Size = new System.Drawing.Size(192, 61);
+            this.button_del.TabIndex = 3;
+            this.button_del.Text = "Удалить";
+            this.button_del.UseVisualStyleBackColor = false;
+            this.button_del.Click += new System.EventHandler(this.button_del_Click);
+            // 
+            // textBox_nomer_alboma
+            // 
+            this.textBox_nomer_alboma.Location = new System.Drawing.Point(335, 292);
+            this.textBox_nomer_alboma.Name = "textBox_nomer_alboma";
+            this.textBox_nomer_alboma.Size = new System.Drawing.Size(100, 26);
+            this.textBox_nomer_alboma.TabIndex = 4;
+            // 
+            // textBox_polz
+            // 
+            this.textBox_polz.Location = new System.Drawing.Point(335, 353);
+            this.textBox_polz.Name = "textBox_polz";
+            this.textBox_polz.Size = new System.Drawing.Size(100, 26);
+            this.textBox_polz.TabIndex = 5;
+            // 
+            // textBox_name
+            // 
+            this.textBox_name.Location = new System.Drawing.Point(335, 413);
+            this.textBox_name.Name = "textBox_name";
+            this.textBox_name.Size = new System.Drawing.Size(100, 26);
+            this.textBox_name.TabIndex = 6;
+            // 
+            // textBox_data
+            // 
+            this.textBox_data.Location = new System.Drawing.Point(335, 475);
+            this.textBox_data.Name = "textBox_data";
+            this.textBox_data.Size = new System.Drawing.Size(100, 26);
+            this.textBox_data.TabIndex = 7;
+            // 
+            // label_nomer_alboma
+            // 
+            this.label_nomer_alboma.AutoSize = true;
+            this.label_nomer_alboma.Font = new System.Drawing.Font("MV Boli", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label_nomer_alboma.Location = new System.Drawing.Point(63, 292);
+            this.label_nomer_alboma.Name = "label_nomer_alboma";
+            this.label_nomer_alboma.Size = new System.Drawing.Size(192, 37);
+            this.label_nomer_alboma.TabIndex = 8;
+            this.label_nomer_alboma.Text = "№ альбома";
+            // 
+            // label_polz
+            // 
+            this.label_polz.AutoSize = true;
+            this.label_polz.Font = new System.Drawing.Font("MV Boli", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label_polz.Location = new System.Drawing.Point(63, 353);
+            this.label_polz.Name = "label_polz";
+            this.label_polz.Size = new System.Drawing.Size(228, 37);
+            this.label_polz.TabIndex = 9;
+            this.label_polz.Text = "Пользователь";
+            // 
+            // label_name
+            // 
+            this.label_name.AutoSize = true;
+            this.label_name.Font = new System.Drawing.Font("MV Boli", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label_name.Location = new System.Drawing.Point(63, 413);
+            this.label_name.Name = "label_name";
+            this.label_name.Size = new System.Drawing.Size(162, 37);
+            this.label_name.TabIndex = 10;
+            this.label_name.Text = "Название";
+            // 
+            // label_data
+            // 
+            this.label_data.AutoSize = true;
+            this.label_data.Font = new System.Drawing.Font("MV Boli", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label_data.Location = new System.Drawing.Point(63, 465);
+            this.label_data.Name = "label_data";
+            this.label_data.Size = new System.Drawing.Size(247, 37);
+            this.label_data.TabIndex = 11;
+            this.label_data.Text = "Дата создания";
+            // 
+            // Form_albom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(800, 531);
+            this.Controls.Add(this.label_data);
+            this.Controls.Add(this.label_name);
+            this.Controls.Add(this.label_polz);
+            this.Controls.Add(this.label_nomer_alboma);
+            this.Controls.Add(this.textBox_data);
+            this.Controls.Add(this.textBox_name);
+            this.Controls.Add(this.textBox_polz);
+            this.Controls.Add(this.textBox_nomer_alboma);
+            this.Controls.Add(this.button_del);
+            this.Controls.Add(this.button_red);
+            this.Controls.Add(this.button_add);
+            this.Controls.Add(this.dataGridView_albom);
+            this.Name = "Form_albom";
+            this.Text = "Альбомы";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_albom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.albumsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.photoalbumDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView_albom;
+        private PhotoalbumDataSet photoalbumDataSet;
+        private System.Windows.Forms.BindingSource albumsBindingSource;
+        private PhotoalbumDataSetTableAdapters.AlbumsTableAdapter albumsTableAdapter;
+        private System.Windows.Forms.Button button_add;
+        private System.Windows.Forms.Button button_red;
+        private System.Windows.Forms.Button button_del;
+        private System.Windows.Forms.TextBox textBox_nomer_alboma;
+        private System.Windows.Forms.TextBox textBox_polz;
+        private System.Windows.Forms.TextBox textBox_name;
+        private System.Windows.Forms.TextBox textBox_data;
+        private System.Windows.Forms.Label label_nomer_alboma;
+        private System.Windows.Forms.Label label_polz;
+        private System.Windows.Forms.Label label_name;
+        private System.Windows.Forms.Label label_data;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Fotoalbom
+{
+    public partial class Form_albom : Form
+    {
+        private string connectionString;
+        private SqlDataAdapter dataAdapter;
+        private DataTable dataTable;
+
+        public Form_albom()
+        {
+            InitializeComponent();
+            connectionString = "Data Source=MICKUCHAAN;Initial Catalog=Photoalbum;Integrated Security=True";
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                dataAdapter = new SqlDataAdapter(@"
+            SELECT Albums.id_albom, Albums.title, Albums.date_create, Users.username
+            FROM Albums
+            JOIN Users ON Albums.users_id = Users.id_user", connection);
+                dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+                dataGridView_albom.DataSource = dataTable;
+
+                dataGridView_albom.Columns["id_albom"].HeaderText = "Альбом";
+                dataGridView_albom.Columns["title"].HeaderText = "Название";
+                dataGridView_albom.Columns["date_create"].HeaderText = "Дата создания";
+                dataGridView_albom.Columns["username"].HeaderText = "Пользователь";
+            }
+        }
+    
+        private void button_add_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string username = textBox_polz.Text;
+                string userIdQuery = "SELECT id_user FROM Users WHERE username = @username";
+
+                int usersId;
+
+                using (SqlCommand userIdCommand = new SqlCommand(userIdQuery, connection))
+                {
+                    userIdCommand.Parameters.AddWithValue("@username", username);
+                    object result = userIdCommand.ExecuteScalar();
+
+                    if (result != null)
+                    {
+                        usersId = Convert.ToInt32(result);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Пользователь с таким именем не найден.");
+                        return;
+                    }
+                }
+
+                string query = "INSERT INTO Albums(id_albom , users_id, title, date_create) VALUES(@id_albom, @users_id, @title, @date_create)";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    string input1 = textBox_nomer_alboma.Text;
+                    string input3 = textBox_name.Text;
+                    string input4 = textBox_data.Text;
+
+                    if (!string.IsNullOrWhiteSpace(input1) &&
+                        !string.IsNullOrWhiteSpace(username) &&
+                        !string.IsNullOrWhiteSpace(input3) &&
+                        !string.IsNullOrWhiteSpace(input4))
+                    {
+                        foreach (DataRow row in dataTable.Rows)
+                        {
+                            if (row["id_albom"].ToString() == input1)
+                            {
+                                MessageBox.Show("Альбом с таким ID уже существует.");
+                                return;
+                            }
+                        }
+
+                        command.Parameters.AddWithValue("@id_albom", input1);
+                        command.Parameters.AddWithValue("@users_id", usersId);
+                        command.Parameters.AddWithValue("@title", input3);
+                        command.Parameters.AddWithValue("@date_create", input4);
+
+                        command.ExecuteNonQuery();
+
+                        DataRow newRow = dataTable.NewRow();
+                        newRow["id_albom"] = input1;
+                        newRow["username"] = username;
+                        newRow["title"] = input3;
+                        newRow["date_create"] = input4;
+
+                        dataTable.Rows.Add(newRow);
+
+                        MessageBox.Show("Альбом добавлен успешно.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Пожалуйста, заполните все поля перед добавлением.");
+                    }
+                }
+            }
+        }
+
+        private void button_del_Click(object sender, EventArgs e)
+        {
+            if (dataGridView_albom.SelectedRows.Count > 0)
+            {
+                var selectedRow = dataGridView_albom.SelectedRows[0];
+                string idToDelete = selectedRow.Cells["id_albom"].Value.ToString();
+
+                DeleteFromDatabase(idToDelete);
+                LoadData();
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, выберите запись для удаления.");
+            }
+        }
+        private void DeleteFromDatabase(string id)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "DELETE FROM Albums WHERE id_albom = @id_albom";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@id_albom", id);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+
+        private void button_red_Click(object sender, EventArgs e)
+        {
+            if (dataGridView_albom.SelectedRows.Count > 0)
+            {
+                var selectedRow = dataGridView_albom.SelectedRows[0];
+                string idToEdit = selectedRow.Cells["id_albom"].Value.ToString(); 
+
+                string userIdQuery = "SELECT id_user FROM Users WHERE username = @username";
+                int usersId;
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand userIdCommand = new SqlCommand(userIdQuery, connection))
+                    {
+                        userIdCommand.Parameters.AddWithValue("@username", textBox_polz.Text);
+                        object result = userIdCommand.ExecuteScalar();
+
+                        if (result != null)
+                        {
+                            usersId = Convert.ToInt32(result);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Пользователь с таким именем не найден.");
+                            return;
+                        }
+                    }
+
+                    string updateQuery = "UPDATE Albums SET users_id = @users_id, title = @title, date_create = @date_create WHERE id_albom = @id_albom";
+                    using (SqlCommand command = new SqlCommand(updateQuery, connection))
+                    {
+                        command.Parameters.AddWithValue("@id_albom", idToEdit);
+                        command.Parameters.AddWithValue("@users_id", usersId);
+                        command.Parameters.AddWithValue("@title", textBox_name.Text);
+                        command.Parameters.AddWithValue("@date_create", textBox_data.Text);
+
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Альбом изменён успешно.");
+                            LoadData();
+                        }
+                        else
+                        {
+                            MessageBox.Show("Не удалось изменить альбом. Проверьте данные.");
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, выберите альбом для изменения.");
+            }
+        }
+    }
+}
